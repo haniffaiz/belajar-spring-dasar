3f363be5359ec3ede05db94318a4e0e22b2d1978
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-25 02:54:37 +0700

    membuat scope manual

--- src/main/java/programmerzamannow/spring/core/ScopeConfiguration.java
+++ src/main/java/programmerzamannow/spring/core/ScopeConfiguration.java
@@ -1,10 +1,13 @@
 package programmerzamannow.spring.core;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import programmerzamannow.spring.core.data.Bar;
 import programmerzamannow.spring.core.data.Foo;
+import programmerzamannow.spring.core.scope.DoubletonScope;
 
 @Slf4j
 @Configuration
@@ -12,7 +15,21 @@ public class ScopeConfiguration {
     @Bean
     @Scope("prototype") //penentu scope default singleton
     public Foo foo(){
-        log.info("");
+        log.info("Create new Foo");
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar(){
+        log.info("Create new Bar");
+        return new Bar();
     }
 }
