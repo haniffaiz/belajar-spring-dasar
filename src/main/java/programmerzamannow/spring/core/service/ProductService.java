e7746a546a7cd7c340aa2b8c1b472a834c97b318
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-25 04:27:18 +0700

    component

---
+++ src/main/java/programmerzamannow/spring/core/service/ProductService.java
@@ -0,0 +1,11 @@
+package programmerzamannow.spring.core.service;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+//@Scope("prototype")
+//@Lazy
+@Component
+public class ProductService {
+}
