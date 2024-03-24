2b8fb35b5fac70afd02a5ddb04c1e4bb5d72c8a2
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-24 19:03:53 +0700

    Initial commit

---
+++ src/main/java/programmerzamannow/spring/core/DependencyInjectionConfiguration.java
@@ -0,0 +1,31 @@
+package programmerzamannow.spring.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import programmerzamannow.spring.core.data.Bar;
+import programmerzamannow.spring.core.data.Foo;
+import programmerzamannow.spring.core.data.FooBar;
+
+@Configuration
+public class DependencyInjectionConfiguration {
+
+    @Bean
+    public Foo fooFirst(){
+        return new Foo();
+    }
+    @Bean
+    public Foo fooSecond(){
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar(){
+        return new Bar();
+    }
+
+    @Bean
+    public FooBar fooBar(Foo foo, Bar bar){
+        return new FooBar(foo, bar);
+    }
+
+}
