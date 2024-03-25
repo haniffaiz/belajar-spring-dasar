6a02e7e0aeae9c53257370212d2d6caeefc443d9
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-25 22:06:39 +0700

    startup failure

--- src/main/java/programmerzamannow/spring/core/application/FooApplication.java
+++ src/main/java/programmerzamannow/spring/core/application/FooApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import programmerzamannow.spring.core.data.Bar;
 import programmerzamannow.spring.core.data.Foo;
 
 @SpringBootApplication
