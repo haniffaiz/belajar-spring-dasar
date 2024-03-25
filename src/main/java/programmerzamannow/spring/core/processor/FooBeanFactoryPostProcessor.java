e4a42b358d268185df144c2e2c6ed5b7462414f7
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-25 21:24:35 +0700

    bean factory post processor

---
+++ src/main/java/programmerzamannow/spring/core/processor/FooBeanFactoryPostProcessor.java
@@ -0,0 +1,24 @@
+package programmerzamannow.spring.core.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import programmerzamannow.spring.core.data.Foo;
+
+public class FooBeanFactoryPostProcessor implements BeanDefinitionRegistryPostProcessor {
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        GenericBeanDefinition definition = new GenericBeanDefinition();
+        definition.setScope("singleton");
+        definition.setBeanClass(Foo.class);
+
+        registry.registerBeanDefinition("foo", definition);
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        //nothing
+    }
+}
